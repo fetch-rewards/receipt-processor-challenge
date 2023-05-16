4bec266457504bf88d84448a233e29f5b6173640
Tony Manion <tonymanion@live.com>
2023-05-15 20:15:56 -0500

    created basic api with get and create receipt endpoints

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type item struct {
+	ShortDescription string `json:"shortDescription"`
+	Price            string `json:"price"`
+}
+
+type receipt struct {
+	ID           int     `json:"id"`
+	Retailer     string  `json:"retailer"`
+	PurchaseDate string  `json:"purchaseDate"`
+	PurchaseTime string  `json:"purchaseTime"`
+	Total        string  `json:"total"`
+	Items        []*item `json:"items"`
+}
+
+var receipts []receipt
+
+func getReciepts(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, receipts)
+}
+
+func createReceipt(c *gin.Context) {
+	var newReceipt receipt
+	if err := c.BindJSON(&newReceipt); err != nil {
+		return
+	}
+	newReceipt.ID = len(receipts) + 1
+
+	receipts = append(receipts, newReceipt)
+	c.IndentedJSON(http.StatusCreated, newReceipt)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/receipts", getReciepts)
+	router.POST("/receipts", createReceipt)
+	router.Run("localhost:8080")
+}
