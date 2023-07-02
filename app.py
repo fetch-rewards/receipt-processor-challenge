081662798bfb602a2b75f31fb2cb069a8e4361cb
giffinsuresh <gs3219@columbia.edu>
2023-07-02 15:33:13 -0400

    Add final solution

--- app.py
+++ app.py
@@ -8,75 +8,65 @@ app = Flask(__name__)
 
 receipts = {}
 
-def check_condition1(receipt):
-    retailer_name = receipt['retailer']
-    points = sum(1 for char in retailer_name if char.isalnum())
-    return points
+class Scorer:
 
-def check_condition2(receipt):
-    points = 0
-    total = float(receipt['total'])
-    if total.is_integer():
-        points += 50
-    return points
+    @classmethod
+    def compute_score(cls, receipt):
+        points = 0
+        points += cls._alpha_numeric_score(receipt)
+        points += cls._is_integer_total_score(receipt)
+        points += cls._is_total_divisible_by_quarter_score(receipt)
+        points += cls._num_items_score(receipt)
+        points += cls._is_trimmed_length_divisible_by_3_score(receipt)
+        points += cls._is_day_odd_score(receipt)
+        points += cls._is_time_between_2_and_4_score(receipt)
+        return points
 
-def check_condition3(receipt):
-    points = 0
-    total = float(receipt['total'])
-    if total % 0.25 == 0:
-        points += 25
-    return points
+    @staticmethod
+    def _alpha_numeric_score(receipt):
+        retailer_name = receipt["retailer"]
+        return sum([1 for char in retailer_name if char.isalnum()])
 
-def check_condition4(receipt):
-    return (len(receipt['items']) // 2) * 5
+    @staticmethod
+    def _is_integer_total_score(receipt):
+        total = float(receipt["total"])
+        return 50 if total.is_integer() else 0
+    
+    @staticmethod
+    def _is_total_divisible_by_quarter_score(receipt):
+        total = float(receipt["total"])
+        return 25 if total % 0.25 == 0 else 0
 
-def check_condition5(receipt):
-    points = 0
-    for item in receipt['items']:
-        trimmed_len = len(item['shortDescription'].strip())
-        if trimmed_len % 3 == 0:
-            price = float(item['price'])
-            points += math.ceil(price * 0.2)
-    return points
+    @staticmethod
+    def _num_items_score(receipt):
+        return (len(receipt['items']) // 2) * 5
 
-def check_condition6(receipt):
-    points = 0
-    purchase_date = datetime.strptime(receipt['purchaseDate'], '%Y-%m-%d')
-    if purchase_date.day % 2 == 1:
-        points += 6
-    return points
+    @staticmethod
+    def _is_trimmed_length_divisible_by_3_score(receipt):
+        points = 0
+        for item in receipt['items']:
+            trimmed_len = len(item['shortDescription'].strip())
+            if trimmed_len % 3 == 0:
+                price = float(item['price'])
+                points += math.ceil(price * 0.2)
+        return points
 
-def check_condition7(receipt):
-    points = 0
-    purchase_time = datetime.strptime(receipt['purchaseTime'], '%H:%M').time()
-    if time(14, 0) < purchase_time < time(16, 0):
-        points += 10
-    return points
-
-def calculate_points(receipt):
-    points = 0
-    points += check_condition1(receipt)
-    points += check_condition2(receipt)
-    points += check_condition3(receipt)
-    points += check_condition4(receipt)
-    points += check_condition5(receipt)
-    points += check_condition6(receipt)
-    points += check_condition7(receipt)
-
-    return points
+    @staticmethod
+    def _is_day_odd_score(receipt):
+        purchase_date = datetime.strptime(receipt["purchaseDate"], "%Y-%m-%d")
+        return 6 if purchase_date.day % 2 == 1 else 0
+    
+    @staticmethod
+    def _is_time_between_2_and_4_score(receipt):
+        purchase_time = datetime.strptime(receipt['purchaseTime'], '%H:%M').time()
+        return 10 if time(14) < purchase_time < time(16) else 0
 
 @app.route('/receipts/process', methods=['POST'])
 def process_receipts():
+    #Get json object, generate id and store in dictionary
     receipt = request.get_json()
-
-    # Validate receipt data
-    if not is_valid_receipt(receipt):
-        return jsonify({'error': 'Invalid receipt data'}), 400
-
     receipt_id = str(uuid.uuid4())
-
     receipts[receipt_id] = receipt
-
     response = {'id': receipt_id}
 
     return jsonify(response), 200
@@ -87,50 +77,11 @@ def get_points(receipt_id):
     if receipt is None:
         return jsonify({'error': 'Receipt not found'}), 404
 
-    points = calculate_points(receipt)
+    #Compute score for the recipt id
+    points = Scorer.compute_score(receipt)
     response = {'points': points}
 
     return jsonify(response), 200
-
-def is_valid_receipt(receipt):
-    # Validate retailer name
-    if not isinstance(receipt['retailer'], str):
-        return False
-
-    # Validate purchase date
-    try:
-        datetime.strptime(receipt['purchaseDate'], '%Y-%m-%d')
-    except ValueError:
-        return False
-
-    # Validate purchase time
-    try:
-        datetime.strptime(receipt['purchaseTime'], '%H:%M')
-    except ValueError:
-        return False
-
-    # Validate items
-    if not isinstance(receipt['items'], list) or len(receipt['items']) < 1:
-        return False
-
-    for item in receipt['items']:
-        if not is_valid_item(item):
-            return False
-
-    # Validate total
-    if not isinstance(receipt['total'], str) or not bool(re.match(r'^\d+\.\d{2}$', receipt['total'])):
-        return False
-
-    return True
-
-def is_valid_item(item):
-    if not isinstance(item['shortDescription'], str) or not bool(re.match(r'^[\w\s\-]+$', item['shortDescription'])):
-        return False
-
-    if not isinstance(item['price'], str) or not bool(re.match(r'^\d+\.\d{2}$', item['price'])):
-        return False
-
-    return True
 
 def get_receipt(receipt_id):
     return receipts.get(receipt_id)
