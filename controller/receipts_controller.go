e226c610f940224f8c280ee223759546ed403de1
dkanney <dkanney@terpmail.umd.edu>
2024-09-19 12:54:25 -0400

    :sparkles: Finish handlers & logic for Receipt/Point CRUD

--- controller/receipts_controller.go
+++ controller/receipts_controller.go
@@ -1,7 +1,11 @@
 package controller
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
+	"receipt-processor-challenge/domain"
 	"receipt-processor-challenge/logic"
 
 	"github.com/gin-gonic/gin"
@@ -19,16 +23,81 @@ func NewReceiptsController(receiptsService logic.IReceiptsService) *ReceiptContr
 
 func (h *ReceiptController) AddRoutes(router *gin.Engine) {
 	receipts := router.Group("/receipts")
-	receipts.POST("/process", h.ProcessReceipts)
-	receipts.POST("/:id/points", h.ProcessReceiptPoints)
+	receipts.POST("/process", h.ProcessReceipt)
+	receipts.GET("/:id/points", h.GetReceiptPoints)
 }
 
-// ProcessReceipts TODO:
-func (h *ReceiptController) ProcessReceipts(c *gin.Context) {
-	c.JSON(http.StatusOK, "receipts processed")
+// ProcessReceipts stores a Receipt and the points calculated from the receipt.
+func (h *ReceiptController) ProcessReceipt(c *gin.Context) {
+	// Parse receipt
+	bodyBytes, err := io.ReadAll(c.Request.Body)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": fmt.Sprintf("Failed to read request body. Error: %v", err.Error()),
+		})
+		return
+	}
+	var receipt domain.Receipt
+	if err = json.Unmarshal(bodyBytes, &receipt); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "The receipt is invalid",
+		})
+		return
+	}
+
+	// Process receipt
+	points := h.receiptsService.ProcessReceipt(&receipt)
+	scoredReceipt := domain.ReceiptScore{
+		Receipt: receipt,
+		Points:  points,
+	}
+
+	// Store scored receipt
+	id, err := h.receiptsService.StoreScoredReceipt(&scoredReceipt)
+	if err != nil {
+		// failureMsg := "There was an error processing the receipt"
+		failureMsg := "The receipt is invalid"
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": fmt.Sprintf("%v. Error: %v", failureMsg, err.Error()),
+		})
+		return
+	}
+
+	response := domain.IDResponse{ID: id}
+
+	c.JSON(http.StatusOK, response)
 }
 
-// ProcessReceiptPoints TODO:
-func (h *ReceiptController) ProcessReceiptPoints(c *gin.Context) {
-	c.JSON(http.StatusOK, "receipt points processed")
+// GetReceiptPoints returns the points calculated by the id's receipt
+func (h *ReceiptController) GetReceiptPoints(c *gin.Context) {
+	id := c.Param("id")
+	if id == "" {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Invalid id provided",
+		})
+	}
+
+	var points uint
+	_, err := h.receiptsService.GetByID(id)
+	if err == nil {
+		points, err = h.receiptsService.GetPointsByID(id)
+	}
+	if err != nil {
+		if err == domain.ErrReceiptNotFound {
+			c.JSON(http.StatusNotFound, gin.H{
+				"error": "No receipt found for that id",
+			})
+			return
+		}
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": fmt.Sprintf("Failed to retrieve points for receipt id: %v. Error: %v", id, err),
+		})
+		return
+	}
+
+	response := domain.PointsResponse{
+		Points: points,
+	}
+
+	c.JSON(http.StatusOK, response)
 }
--- logic/receipts_service.go
+++ logic/receipts_service.go
@@ -1,20 +1,101 @@
 package logic
 
-import "receipt-processor-challenge/domain"
+import (
+	"receipt-processor-challenge/domain"
+	"receipt-processor-challenge/mysqlrepo"
+	"strings"
+	"time"
+	"unicode"
+
+	"github.com/shopspring/decimal"
+)
 
 type IReceiptsService interface {
-	ByID(id uint) domain.Receipt
+	ProcessReceipt(receipt *domain.Receipt) (points uint)
+	StoreReceipt(receipt *domain.Receipt) (id string)
+	StoreScoredReceipt(receiptScore *domain.ReceiptScore) (id string, err error)
+	GetByID(id string) (domain.Receipt, error)
+	GetPointsByID(id string) (uint, error)
 }
 
 type ReceiptsService struct {
+	receiptService mysqlrepo.ReceiptRepo
 }
 
 func NewReceiptsService() *ReceiptsService {
 	return &ReceiptsService{}
 }
 
-func (s *ReceiptsService) ByID(id uint) domain.Receipt {
-	return domain.Receipt{
-		ID: id,
+func (s *ReceiptsService) ProcessReceipt(receipt *domain.Receipt) (points uint) {
+	// One point for every alphanumeric character in the retailer name.
+	points += s.countAlphanumericCharacters(receipt.RetailerName)
+
+	// 50 points if the total is a round dollar amount with no cents (total == math.ceil(total))
+	if receipt.Total.Equal(receipt.Total.Ceil()) {
+		points += 50
 	}
+
+	// 25 points if the total is a multiple of 0.25 (total % 0.25 == 0)
+	if receipt.Total.Mod(decimal.NewFromFloat32(0.25)).Equal(decimal.Zero) {
+		points += 25
+	}
+
+	// 5 points for every two items on the receipt (len(items) / 2 * 5)
+	itemCouples := (len(receipt.Items) / 2)
+	points += uint(itemCouples * 5)
+
+	// If the trimmed length of the item description is a multiple of 3, multiply the price by 0.2 and round up to the nearest integer.
+	// The result is the number of points earned.
+	var scalar = decimal.NewFromFloat32(0.2)
+	for _, item := range receipt.Items {
+		if len(strings.TrimSpace(item.ShortDescription))%3 == 0 {
+			points += uint(item.Price.Mul(scalar).Ceil().IntPart())
+		}
+	}
+
+	// 6 points if the day in the purchase date is odd.
+	if receipt.PurchaseDate.Day()%2 != 0 {
+		points += 6
+	}
+
+	// 10 points if the time of purchase is after 2:00pm (14:00) and before 4:00pm (16:00)
+	start, _ := time.Parse("15:04", "14:00")
+	end, _ := time.Parse("15:04", "16:00")
+	if receipt.PurchaseTime.IsBetween(start, end) {
+		points += 10
+	}
+
+	return
+}
+
+func (s *ReceiptsService) StoreReceipt(receipt *domain.Receipt) (id string) {
+	return s.receiptService.Store(*receipt)
+}
+
+func (s *ReceiptsService) StoreScoredReceipt(receiptScore *domain.ReceiptScore) (id string, err error) {
+	if receiptScore == nil {
+		return "", nil
+	}
+
+	// tx
+	id = s.StoreReceipt(&receiptScore.Receipt)
+	err = s.receiptService.StorePoints(id, receiptScore.Points)
+	return
+}
+
+func (s *ReceiptsService) GetByID(id string) (domain.Receipt, error) {
+	return s.receiptService.GetByID(id)
+}
+
+func (s *ReceiptsService) GetPointsByID(id string) (uint, error) {
+	return s.receiptService.GetPointsByID(id)
+}
+
+func (s *ReceiptsService) countAlphanumericCharacters(str string) (count uint) {
+	for _, char := range str {
+		if unicode.IsLetter(char) {
+			count++
+		}
+	}
+	return
 }
