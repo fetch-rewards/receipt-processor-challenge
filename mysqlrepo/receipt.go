523fe714a49d85cec6691baafd7dc0b647be903e
dkanney <dkanney@terpmail.umd.edu>
2024-09-19 12:53:20 -0400

    :card_file_box: Create repo layer for Receipt/Point read/write

---
+++ mysqlrepo/receipt.go
@@ -0,0 +1,52 @@
+package mysqlrepo
+
+import (
+	"receipt-processor-challenge/domain"
+
+	"github.com/google/uuid"
+)
+
+var (
+	receiptsTable = make(map[string]domain.Receipt)
+	pointsTable   = make(map[string]uint)
+)
+
+type ReceiptRepo struct{}
+
+func (s *ReceiptRepo) Store(receipt domain.Receipt) string {
+	id := s.GenerateUUID()
+	receiptsTable[id] = receipt
+	return id
+}
+
+func (s *ReceiptRepo) StorePoints(id string, points uint) error {
+	if _, exists := receiptsTable[id]; !exists {
+		// no receipt exists in the system for this id
+		return domain.ErrReceiptNotFound
+	}
+	pointsTable[id] = points
+
+	return nil
+}
+
+func (s *ReceiptRepo) GetByID(id string) (domain.Receipt, error) {
+	result, ok := receiptsTable[id]
+	if !ok {
+		return domain.Receipt{}, domain.ErrReceiptNotFound
+	}
+
+	return result, nil
+}
+
+func (s *ReceiptRepo) GetPointsByID(id string) (uint, error) {
+	result, ok := pointsTable[id]
+	if !ok {
+		return 0, domain.ErrReceiptNotFound
+	}
+
+	return result, nil
+}
+
+func (s *ReceiptRepo) GenerateUUID() string {
+	return uuid.New().String()
+}
