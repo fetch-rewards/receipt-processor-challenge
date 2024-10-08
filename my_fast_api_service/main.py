b194e6e8c23e5655aef908ed5e8559f715aef75b
Kevin Cuadros <kev040798@gmail.com>
2024-10-07 22:31:06 -0400

    Update main with api_router and exception handler for bad requests

--- my_fast_api_service/main.py
+++ my_fast_api_service/main.py
@@ -1,8 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, Request
+from api.routes import api_router
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
 
-app = FastAPI(title="Receipt Processor")
+def include_router(app):
+    app.include_router(api_router)
 
-@app.get("/")
-def welome():
-    return {"msg": "Hello, world!"}
+def start_services():
+    app = FastAPI(title="Receipt Processor")
+
+    include_router(app)
+
+    return app
+
+
+app = start_services()
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    return JSONResponse(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        content={"detail": exc.errors()},
+    )
+
 
