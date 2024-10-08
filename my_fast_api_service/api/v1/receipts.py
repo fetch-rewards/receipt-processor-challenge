926252e6237043324e42642a975270ff52e4b564
Kevin Cuadros <kev040798@gmail.com>
2024-10-08 16:59:46 -0400

    Add receipt storage

--- my_fast_api_service/api/v1/receipts.py
+++ my_fast_api_service/api/v1/receipts.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, status
 
 from pydanctic_models.receipt_process import ReceiptProcessCreate, ShowReceiptProcess, ReceiptProcess
 from api.helpers.receipt_process import new_receipt_process
+from data_store import receipts_storage
 
 router = APIRouter()
 
@@ -9,5 +10,7 @@ router = APIRouter()
 @router.post("/api/v1/receipts/process", response_model=ShowReceiptProcess, status_code=status.HTTP_201_CREATED)
 def create_receipt_process(receipt_process: ReceiptProcessCreate):
     receipt_process = new_receipt_process(receipt_process, ReceiptProcess)
+
+    receipts_storage.append({receipt_process.id: receipt_process})
 
     return receipt_process
