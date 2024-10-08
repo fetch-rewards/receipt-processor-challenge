b633f8ca0d105bf6fb7b1e11d8df241b0b19fdad
Kevin Cuadros <kev040798@gmail.com>
2024-10-07 22:38:04 -0400

    Add sad path test

--- my_fast_api_service/tests/test_api_endpoints/test_receipts.py
+++ my_fast_api_service/tests/test_api_endpoints/test_receipts.py
@@ -18,3 +18,21 @@ def test_create_receipt_process():
 
     assert response.status_code == 201
     assert response.json()["id"]
+
+def test_create_receipt_error():
+    payload = {
+        "purchase_date": "2024-10-08",
+        "purchase_time": "13:01",
+        "items": [
+            {
+                "short_description": "Grocery Store Visit",
+                "price": "12.99"
+            }
+        ],
+        "total": "6.49"
+    }
+
+    response = client.post('/api/v1/receipts/process', json=payload)
+
+    assert response.status_code == 400
+    assert response.json()['detail']
