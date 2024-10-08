fa08b31eec6e3f0ed7250ff574437cfd0a6168fe
Kevin Cuadros <kev040798@gmail.com>
2024-10-07 22:34:07 -0400

    Add initial unit tests for api and with config

---
+++ my_fast_api_service/tests/test_api_endpoints/test_receipts.py
@@ -0,0 +1,20 @@
+from tests.test_config import client
+
+def test_create_receipt_process():
+    payload = {
+        "retailer": "M&M Corner Market",
+        "purchase_date": "2024-10-08",
+        "purchase_time": "13:01",
+        "items": [
+            {
+                "short_description": "Grocery Store Visit",
+                "price": "12.99"
+            }
+        ],
+        "total": "6.49"
+    }
+
+    response = client.post('/api/v1/receipts/process', json=payload)
+
+    assert response.status_code == 201
+    assert response.json()["id"]
